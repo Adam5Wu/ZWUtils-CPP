e62d397b989cb0aa8e20a50ed638033b37e521c1
Zhenyu Wu <adamwu@nec-labs.com>
2018-10-17 18:46:43 -0400

    Add utility macros to make code cleaner

--- main/src/cpp/Misc/Global.h
+++ main/src/cpp/Misc/Global.h
@@ -71,6 +71,41 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define STRINGIZE(x) STRINGIZE_HELPER(x)
 #define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") WARNING: " desc)
 
+#if defined(_M_IX86)
+#define __LINKER_SYMBOL(name)	_ ## name
 #endif
+#if defined(_M_AMD64)
+#define __LINKER_SYMBOL(name)	name
+#endif
+
+#endif
+
+#define __EXPAND(x)		x
+#define __PAREN(...)	(__VA_ARGS__)
+
+#define __NUM_ARGS_SELECTOR(x16, x15, x14, x13, x12, x11, x10, x9, x8, x7, x6, x5, x4, x3, x2, x1, x0, ...) x0
+#define __NUM_ARGS(...) __EXPAND(__NUM_ARGS_SELECTOR(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
+
+#define __APPLY_0(t, dummy)
+#define __APPLY_1(t, a)					t(a)
+#define __APPLY_2(t, a, b)				t(a) t(b)
+#define __APPLY_3(t, a, b, c)			t(a) t(b) t(c)
+#define __APPLY_4(t, a, b, c, d)		t(a) t(b) t(c) t(d)
+#define __APPLY_5(t, a, b, c, d, e)				__APPLY_4(t, a, b, c, d)  __APPLY_1(t, e)
+#define __APPLY_6(t, a, b, c, d, e, f)			__APPLY_4(t, a, b, c, d)  __APPLY_2(t, e, f)
+#define __APPLY_7(t, a, b, c, d, e, f, g)		__APPLY_4(t, a, b, c, d)  __APPLY_3(t, e, f, g)
+#define __APPLY_8(t, a, b, c, d, e, f, g, h)	__APPLY_4(t, a, b, c, d)  __APPLY_4(t, e, f, g, h)
+#define __APPLY_9(t, a, b, c, d, e, f, g, h, i)							__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_1(t, i)
+#define __APPLY_10(t, a, b, c, d, e, f, g, h, i, j)						__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_2(t, i, j)
+#define __APPLY_11(t, a, b, c, d, e, f, g, h, i, j, k)					__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_3(t, i, j, k)
+#define __APPLY_12(t, a, b, c, d, e, f, g, h, i, j, k, l)				__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_4(t, i, j, k, l)
+#define __APPLY_13(t, a, b, c, d, e, f, g, h, i, j, k, l, m)			__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_5(t, i, j, k, l, m)
+#define __APPLY_14(t, a, b, c, d, e, f, g, h, i, j, k, l, m, n)			__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_6(t, i, j, k, l, m, n)
+#define __APPLY_15(t, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o)		__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_7(t, i, j, k, l, m, n, o)
+#define __APPLY_16(t, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p)	__APPLY_8(t, a, b, c, d, e, f, g, h)  __APPLY_8(t, i, j, k, l, m, n, o, p)
+
+#define __APPLY_ALL_N(t, n, ...)	__EXPAND(__APPLY_##n(t, __VA_ARGS__))
+#define __APPLY_ALL_WRAP(t, n, ...)	__APPLY_ALL_N(t, n, __VA_ARGS__)
+#define __APPLY_ALL(t, ...)			__APPLY_ALL_WRAP(t, __NUM_ARGS(__VA_ARGS__), __VA_ARGS__)
 
 #endif
